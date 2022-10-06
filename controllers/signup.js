234bc15da36e0f677c984930be5546772efb74de
ahnsucheol <josephscahn@gmail.com>
2022-10-06 12:26:27 +0900

    Refactor: 예외처리 추가 및 리팩토링

--- controllers/signup.js
+++ controllers/signup.js
@@ -2,27 +2,52 @@ const signupServices = require('../services/signup');
 
 const signup = async (req, res) => {
   const { user_name, birth_date, height, phone_number } = req.body;
+
   try {
-    if (typeof user_name !== 'string') {
-      const err = new Error('user_name must be a string');
-      err.status = 400;
-      throw err;
+    const hasKey = {
+      user_name: false,
+      birth_date: false,
+      height: false,
+      phone_number: false,
+    };
+
+    /** 받아온 데이터에 키 + 벨류 값이 존재하는지 확인하는 코드 */
+    const requireKey = Object.keys(hasKey);
+
+    Object.entries(req.body).forEach(([key, value]) => {
+      if (requireKey.includes(key) && value) {
+        hasKey[key] = true;
+      }
+    });
+
+    /** 받아온 데이터에 키 + 벨류 값이 없을때 에러를 표시해주는 코드 */
+    const hasKeyArray = Object.entries(hasKey);
+    for (let i = 0; i < hasKeyArray.length; i++) {
+      const [key, value] = hasKeyArray[i];
+      if (!value) {
+        const err = new Error(`${key} must be entered`);
+        err.status = 400;
+        throw err;
+      }
     }
-    if (typeof birth_date !== 'string') {
-      const err = new Error('birth_date must be a string');
-      err.status = 400;
-      throw err;
-    }
-    if (typeof height !== 'number') {
-      const err = new Error('height must be a number');
-      err.status = 400;
-      throw err;
-    }
-    if (typeof phone_number !== 'string') {
-      const err = new Error('phone_number must be a string');
-      err.status = 400;
-      throw err;
-    }
+
+    /** 받아온 데이터의 타입을 확인해서 틀렸으면 에러를 표시해주는 코드 */
+    Object.entries(req.body).forEach(([key, value]) => {
+      if (key === 'height') {
+        if (typeof value !== 'number') {
+          const err = new Error(`${key} must be a number`);
+          err.status = 400;
+          throw err;
+        }
+      } else {
+        if (typeof value !== 'string') {
+          const err = new Error(`${key} must be a string`);
+          err.status = 400;
+          throw err;
+        }
+      }
+    });
+
     await signupServices.signup(user_name, birth_date, height, phone_number);
     res.status(201).json({ message: 'User SignUp Success' });
   } catch (err) {
